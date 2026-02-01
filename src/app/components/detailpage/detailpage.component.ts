480dffb06c5ee5d0b8e96929832b810f549d272e
manoj@ubuntu <manojkapure100@gmail.com>
2026-02-01 18:46:51 +0000

    connected backend

--- src/app/components/detailpage/detailpage.component.ts
+++ src/app/components/detailpage/detailpage.component.ts
@@ -1,23 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { CapitalMarketServiceService } from '../../services/capital-market-service.service';
+import { CapitalMarketServiceService, FinancialRatioResponse, stockDetail } from '../../services/capital-market-service.service';
 import { map, Observable, of } from 'rxjs';
 import { StockchartComponent } from '../stockchart/stockchart.component';
 import { ComparisonTableComponent } from '../comparison-table/comparison-table.component';
 import { TimeSeriesTableComponent } from '../time-series-table/time-series-table.component';
-
-interface stockDetail {
-  property: string,
-  value: string,
-  valuePrefix: string,
-  valuePostfix: string,
-}
+import { KeyValuePipe, DecimalPipe } from '@angular/common';
 
 @Component({
   selector: 'app-detailpage',
   imports: [
     StockchartComponent, 
     ComparisonTableComponent, 
+    KeyValuePipe,
+    DecimalPipe,
     // TimeSeriesTableComponent
   ],
   templateUrl: './detailpage.component.html',
@@ -28,58 +24,9 @@ export class DetailpageComponent implements OnInit {
   symbol!: string | null;
   company!: string | null;
   companies$
-  stockDetails: stockDetail[] = [
-    {
-      property: 'Market Value',
-      value: '12',
-      valuePrefix: '$',
-      valuePostfix: 'CR'
-    },
-    {
-      property: 'Current price',
-      value: '12',
-      valuePrefix: '$',
-      valuePostfix: 'CR'
-    },
-    {
-      property: 'P / E',
-      value: '12',
-      valuePrefix: '$',
-      valuePostfix: 'CR'
-    },
-    {
-      property: 'Book value',
-      value: '12',
-      valuePrefix: '$',
-      valuePostfix: 'CR'
-    },
-    {
-      property: 'Divident yield',
-      value: '12',
-      valuePrefix: '$',
-      valuePostfix: 'CR'
-    },
-    {
-      property: 'Face value',
-      value: '12',
-      valuePrefix: '$',
-      valuePostfix: 'CR'
-    },
-    {
-      property: 'ROCE',
-      value: '12',
-      valuePrefix: '$',
-      valuePostfix: 'CR'
-    },
-    {
-      property: 'ROE',
-      value: '12',
-      valuePrefix: '$',
-      valuePostfix: 'CR'
-    },
-  ];
+  stockFinancialRatios!: FinancialRatioResponse;
 
-   stockData: any[] = [
+  stockData: any[] = [
     ['2026-01-01', 50],
     ['2026-01-02', 55],
     ['2026-01-03', 60],
@@ -108,6 +55,10 @@ export class DetailpageComponent implements OnInit {
     private capitalMarketService: CapitalMarketServiceService,
   ) {
     this.companies$ = this.capitalMarketService.stockData$;
+    this.initialiseChart();
+  }
+
+  initialiseChart(){
     this.chartOptions = {
       tooltip: { trigger: 'axis' },
       xAxis: { type: 'time', boundaryGap: false },
@@ -126,17 +77,27 @@ export class DetailpageComponent implements OnInit {
 
   ngOnInit(): void {
     let tempParam = this.route.snapshot.paramMap.get('symbol');
-    this.isParamValid(tempParam).subscribe(isValid => {
+    if(!tempParam){
+      this.router.navigate(['home']);
+      return;
+    }
+    
+    this.isParamValid(tempParam).subscribe(async isValid => {
       if (!isValid) {
         this.router.navigate(['home']);
       }
       this.symbol = tempParam;
-      this.updateContent();
+      await this.updateContent(this.symbol);
     });
   }
+  
+  async updateContent(symbol: string) {
+    await this.getFinancialRatio(symbol);
+  }
 
-  updateContent() {
-
+  async getFinancialRatio(symbol: string) {
+    await this.capitalMarketService.getFinancialRatio(symbol);
+    this.stockFinancialRatios = this.capitalMarketService.financialRatio;
   }
 
   isParamValid(params: string | null): Observable<boolean> {
