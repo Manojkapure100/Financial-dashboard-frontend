8b967fa5dea83437cfbe0380a3a942451a1ef8d6
manoj@ubuntu <manojkapure100@gmail.com>
2026-02-16 19:10:49 +0000

    commit need to work

--- src/app/services/capital-market-service.service.ts
+++ src/app/services/capital-market-service.service.ts
@@ -1,23 +1,19 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { firstValueFrom, map, Observable, shareReplay } from 'rxjs';
-
 export interface CompanyInterface {
   name: string,
   symbol: string
 }
-
 interface CompanyResponse {
   data: CompanyInterface[];
 }
-
 export interface stockDetail {
   property: string,
   value: string,
   valuePrefix: string,
   valuePostfix: string,
 }
-
 export interface FinancialRatios {
   // Liquidity
   currentRatio: number | null;
@@ -42,8 +38,6 @@ export interface FinancialRatios {
   inventoryTurnover: number | null;
   freeCashFlowMargin: number | null;
 }
-
-
 export interface YearlyFinancialRatios {
   year: string;
   ratios: FinancialRatios;
@@ -54,13 +48,18 @@ export interface FinancialRatioResponse {
   history: YearlyFinancialRatios[];
 }
 
+export interface Interval {
+  name: string;
+  value: number;
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class CapitalMarketServiceService {
   private jsonUrl = 'all_nse_stocks.json'; // public folder and 'assets/stocks.json'; for assets folder
-  // private readonly baseUrl = 'http://localhost:8000/api/v1'; 
-  private readonly baseUrl = 'https://financial-dashboard-backend-w939.onrender.com/api/v1';
+  private readonly baseUrl = 'http://localhost:8000/api/v1';
+  // private readonly baseUrl = 'https://financial-dashboard-backend-w939.onrender.com/api/v1';
   private stockDetail: any | undefined = [];
   public stockData$: Observable<CompanyInterface[]>;
   _financialRatio!: FinancialRatioResponse;
@@ -84,7 +83,7 @@ export class CapitalMarketServiceService {
   async getCurrentPriceAndPersantage(stockSymbol: string) {
     const endpoint = `/market/stock/${stockSymbol}`;
     const response = await firstValueFrom(this.http.get<any>(`${this.baseUrl}${endpoint}`));
-    if(!response.body.status){
+    if (!response.body.status) {
       throw new Error(response.body.message)
     }
     return response.body.data.fetched[0];
@@ -94,6 +93,22 @@ export class CapitalMarketServiceService {
     const endpoint = `/capitalmarket/company/${stockSymbol}`;
     // Use any instead of any[]
     this.stockDetail = await firstValueFrom(this.http.get<any>(`${this.baseUrl}${endpoint}`));
+  }
+
+  getIntervals(): Observable<Interval[]> {
+    const endpoint = '/market/intervals';
+    return this.http.get<any>(`${this.baseUrl}${endpoint}`);
+  }
+
+  getStockPriceList(symbol: string | null,interval: string,fromdate: string,todate: string) {
+    const endpoint = '/market/stock/priceList';
+    const requestBody = {
+      "symbol": symbol || 'HBLENGINE',
+      "interval": interval || 'ONE_DAY',
+      "fromDate": fromdate,
+      "toDate": todate
+    }
+    return this.http.post<any>(`${this.baseUrl}${endpoint}`, requestBody);
   }
 
   async getFinancialRatio(stockSymbol: string) {
